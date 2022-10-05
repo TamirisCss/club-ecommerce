b362aebe4b47e87effd06973081ae8498fa00f5a
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-10-05 16:48:26 +0200

    feat: add React Hook Form validation to Login Page and Custom Input

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,5 +1,6 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
+import { useForm } from 'react-hook-form'
 
 import CustomButton from '../../components/custom-button/custom-button.component'
 import CustomInput from '../../components/custom-input/custom-input.component'
@@ -14,6 +15,16 @@ import {
 } from './login.style'
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log({ data })
+  }
+
   return (
     <>
       <Header />
@@ -26,14 +37,24 @@ const LoginPage = () => {
           <LoginSubtitle>or use email</LoginSubtitle>
           <LoginInputContainer>
             <p>Email</p>
-            <CustomInput placeholder="email" />
+            <CustomInput
+              hasError={!!errors?.email}
+              placeholder="email"
+              {...register('email', { required: true })}
+            />
           </LoginInputContainer>
           <LoginInputContainer>
             <p>Password</p>
-            <CustomInput placeholder="password" />
+            <CustomInput
+              hasError={!!errors?.password}
+              placeholder="password"
+              {...register('password', { required: true })}
+            />
           </LoginInputContainer>
-          <CustomButton startIcon={<FiLogIn size={18} />}>
-            Login in
+          <CustomButton
+            startIcon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}>
+            Sign In
           </CustomButton>
         </LoginContent>
       </LoginContainer>
