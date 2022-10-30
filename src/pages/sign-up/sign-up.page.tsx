885ce59e0f30c1c1bb487ecbe6aea9ff43fccc12
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-10-30 16:03:52 +0100

    feat: add validation to form

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -21,11 +21,16 @@ interface SignUpForm {
 }
 
 const SignUpPage = () => {
-  const { register, handleSubmit } = useForm<SignUpForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignUpForm>()
 
   const handleSubmitPress = (data: SignUpForm) => {
-    console.log(data)
+    console.log({ data })
   }
+  console.log({ errors })
 
   return (
     <>
@@ -38,43 +43,56 @@ const SignUpPage = () => {
           <SignUpInputContainer>
             <p>Name</p>
             <CustomInput
+              hasError={!!errors?.name}
               placeholder="type your name"
               {...register('name', { required: true })}
             />
+            {errors?.name?.type === 'required' && <p>name required</p>}
           </SignUpInputContainer>
 
           <SignUpInputContainer>
             <p>Second Name</p>
             <CustomInput
+              hasError={!!errors?.lastName}
               placeholder="second name"
               {...register('lastName', { required: true })}
             />
+            {errors?.lastName?.type === 'required' && <p>last Name required</p>}
           </SignUpInputContainer>
 
           <SignUpInputContainer>
             <p>E-mail</p>
             <CustomInput
+              hasError={!!errors?.email}
               placeholder="type your email"
               {...register('email', { required: true })}
             />
+            {errors?.email?.type === 'required' && <p>email required</p>}
+            {errors?.email?.type === 'validate' && <p>use a valid email</p>}
           </SignUpInputContainer>
 
           <SignUpInputContainer>
             <p>Password</p>
             <CustomInput
+              hasError={!!errors?.password}
               placeholder="type your password"
               type="password"
               {...register('password', { required: true })}
             />
+            {errors?.password?.type === 'required' && <p>password required</p>}
           </SignUpInputContainer>
 
           <SignUpInputContainer>
             <p>Confirm your Password</p>
             <CustomInput
+              hasError={!!errors?.passwordConfirmation}
               placeholder="confirm your password"
               type="password"
               {...register('password', { required: true })}
             />
+            {errors?.passwordConfirmation?.type === 'required' && (
+              <p>confirmation of password is required</p>
+            )}
           </SignUpInputContainer>
 
           <CustomButton
