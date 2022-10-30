97813864f1943fc16d4e2594a2b236b2c8bb1d65
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-10-30 16:17:54 +0100

    feat: add typescript interface to LoginPage

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form'
 import CustomButton from '../../components/custom-button/custom-button.component'
 import CustomInput from '../../components/custom-input/custom-input.component'
 import Header from '../../components/header/header.component'
+import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
 
 import {
   LoginContainer,
@@ -13,14 +14,18 @@ import {
   LoginSubtitle,
   LoginInputContainer
 } from './login.style'
-import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
+
+interface LoginForm {
+  email: string
+  password: string
+}
 
 const LoginPage = () => {
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm<LoginForm>()
 
   const handleSubmitPress = (data: any) => {
     console.log({ data })
