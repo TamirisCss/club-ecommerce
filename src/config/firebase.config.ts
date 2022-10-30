5c48e16909d15aeac1b999c9566f0078036a39cc
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-10-30 16:55:50 +0100

    chore: add firebase configs

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDKO0Sq1lSCMrhWZdRQsbsTDCm08fUWEII',
+  authDomain: 'club-ecommerce-49eb4.firebaseapp.com',
+  projectId: 'club-ecommerce-49eb4',
+  storageBucket: 'club-ecommerce-49eb4.appspot.com',
+  messagingSenderId: '723189392702',
+  appId: '1:723189392702:web:80b5f7651be0dd4407786f'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app)
