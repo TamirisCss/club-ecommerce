baa1a523da0a3fec98181961ed7c507ab93f49e1
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-10-31 10:56:25 +0100

    feat: get categories from firestore db

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { getDocs, collection } from 'firebase/firestore'
 
 import Category from '../../types/category.types'
 import CategoryItem from '../category-item/category-item.component'
 import env from '../../config/env.config'
 
 import { CategoriesContainer, CategoriesContent } from './categories.style'
+import { db } from '../../config/firebase.config'
+import { categoryConverter } from '../../converters/firestore.converters'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
-  console.log({ categories })
 
   const fetchCategories = async () => {
     try {
-      const { data } = await axios.get(`${env.apiUrl}/api/category`)
-      setCategories(data)
+      const categoriesFromFirestore: Category[] = []
+
+      const querySnapshot = await getDocs(
+        collection(db, 'categories').withConverter(categoryConverter)
+      )
+
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data())
+      })
+      setCategories(categoriesFromFirestore)
+      // console.log({ categoriesFromFirestore })
     } catch (error) {
       console.log({ error })
     }
